6fe9200460ecdf3942bfbb0305b06770e4a36c54
Marius van Niekerk <marius.v.niekerk@gmail.com>
2017-08-21 08:39:45 -0400

    added default configuration options

--- recipes/freetds/build.sh
+++ recipes/freetds/build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-./configure --prefix $PREFIX
+./configure --prefix $PREFIX --with-tdsver=7.4 --with-openssl
 make
 # To run this check we need to have access to a mssql instance.
 # make check
